dffc4362fa175d4872f6c7b416edc9378bc852a0
Yuvraj Singh <2k23.psitaiml2312687@gmail.com>
2026-08-06 14:46:00 +0530

    Time: 663 ms (27.68%), Space: 63 MB (37.1%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -2,21 +2,21 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        memo={}
+        memo=[[-1]*(amount+1) for _ in range(n+1)]
 
         def Solve(i,ts):
             if ts == 0:
                 return 1
             if i == n:
                 return 0
-            if (i,ts) in memo:
-                return memo[(i,ts)]
+            if memo[i][ts] !=-1:
+                return memo[i][ts]
             take = 0
             if coins[i]<=ts:
                 take = Solve(i,ts-coins[i])
             
             Not_Take=Solve(i+1,ts)
-            memo[(i,ts)]=take+Not_Take
-            return memo[(i,ts)]
+            memo[i][ts]=take+Not_Take
+            return memo[i][ts]
         return Solve(0,amount)
         
