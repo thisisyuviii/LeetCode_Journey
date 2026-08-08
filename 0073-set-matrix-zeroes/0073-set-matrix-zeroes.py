213a451bec457fb05aa97ad390475914904c733e
Yuvraj Singh <2k23.psitaiml2312687@gmail.com>
2026-08-08 10:49:14 +0530

    Time: 7 ms (57.91%), Space: 20.9 MB (13.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,29 +1,30 @@
-#Brute Force
-#Time Complexity: O((m * n) * (m + n))
+#Optimal Approach
+#Time Complexity: O(m * n)
 
 class Solution:
-    def markInfinity(self, matrix,row,col):
-        r=len(matrix)
-        c=len(matrix[0])
-
-        for i in range (r):
-            if matrix[i][col] !=0:
-                matrix[i][col] = float("inf")
-
-        for j in range(c):
-            if matrix[row][j] !=0:
-                matrix[row][j] = float("inf")
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        r=len(matrix)
-        c=len(matrix[0])
+        r = len(matrix)
+        c = len(matrix[0])
+        col0 = 1 
         for i in range(r):
-            for j in range (c):
+            for j in range(c):
                 if matrix[i][j] == 0:
-                    self.markInfinity(matrix,i,j)
-        for i in range(r):
-            for j in range (c):
-                if matrix[i][j] == float("inf"):
-                    matrix[i][j] =0
+                    if j == 0:
+                        col0 = 0  
+                    else:
+                        matrix[0][j] = 0 
+                        matrix[i][0] = 0  
+        for i in range(1, r):
+            for j in range(1, c):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+
+        for j in range(c - 1, 0, -1):
+            if matrix[0][0] == 0:
+                matrix[0][j] = 0
+        for i in range(0, r):
+            if col0 == 0:
+                matrix[i][0] = 0
 
                 
             
