ebda5207aa23bc220c984491cbfd3e3d0160a037
Yuvraj Singh <2k23.psitaiml2312687@gmail.com>
2026-09-10 09:53:48 +0530

    Time: 0 ms (100%), Space: 19.6 MB (32.3%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,7 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        if target in nums:
-            return True
-        else:
-            return False
+        for i in nums:
+            if i==target:
+                return True
+        return False
+
         
